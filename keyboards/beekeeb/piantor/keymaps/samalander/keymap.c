245bf84e376a7e6dd89874383ca161a90b48436e
Eric Greve <eric.greve@ipsa.org>
2023-01-19 21:28:57 -0500

    Swapping the main thumb keys on the Piantor layout.

--- keyboards/beekeeb/piantor/keymaps/samalander/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/samalander/keymap.c
@@ -55,49 +55,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EQL,    KC_Q,    KC_W,    KC_F,    KC_P,     KC_B,                        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
     TD(TD_BRC),HRM_A,   HRM_R,   HRM_S,   HRM_T,    KC_G,                        KC_M,    HRM_N,   HRM_E,   HRM_I,   HRM_O,   TD(TD_QG),
     TD(TD_PRN),KC_Z,    HRM_X,   KC_C,    KC_D,     KC_V,                        KC_K,    KC_H,    KC_COMM, HRM_DT,  KC_SLSH, KC_BSLS,
-                                          LS_MEDIA, LS_NAV, LS_MOUSE,    LS_SYM, LS_NUM,  LS_FUN
+                                          LS_MEDIA, LS_MOUSE, LS_NAV,    LS_NUM, LS_SYM,  LS_FUN
   ),
 
   [_NAV] = LAYOUT_split_3x6_3(
     XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC_AGIN, KC_PSTE, KC_COPY, KC_CUT,  KC_UNDO, XXXXXXX,
     XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                        KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, CW_TOGG,
     XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,                        KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
-                                        XXXXXXX, _______, XXXXXXX,      KC_ENT,  KC_BSPC, KC_DEL
+                                        XXXXXXX, XXXXXXX, _______,      KC_BSPC, KC_ENT,  KC_DEL
   ),
 
   [_MOUSE] = LAYOUT_split_3x6_3(
     XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC_AGIN, KC_PSTE, KC_COPY, KC_CUT,  KC_UNDO, XXXXXXX,
     XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                        XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX,
     XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX,
-                                        XXXXXXX, XXXXXXX, _______,      KC_BTN2, KC_BTN1, KC_BTN3
+                                        XXXXXXX, _______, XXXXXXX,      KC_BTN1, KC_BTN2, KC_BTN3
   ),
 
   [_MEDIA] = LAYOUT_split_3x6_3(
     XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                        XXXXXXX, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX,
     XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                        _______, XXXXXXX, XXXXXXX,      KC_MSTP, KC_MPLY, KC_MUTE
+                                        _______, XXXXXXX, XXXXXXX,      KC_MPLY, KC_MSTP, KC_MUTE
   ),
 
   [_NUM] = LAYOUT_split_3x6_3(
     XXXXXXX, KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX,
     XXXXXXX, KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,                         XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
     XXXXXXX, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,                        XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
-                                        KC_DOT,  KC_0,    KC_MINS,      XXXXXXX, _______, XXXXXXX
+                                        KC_DOT,  KC_MINS, KC_0,         _______, XXXXXXX, XXXXXXX
   ),
 
   [_SYM] = LAYOUT_split_3x6_3(
     XXXXXXX, KC_LCBR, KC_AMPR, KC_ASTR, KC_RPRN, KC_RCBR,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX,
     XXXXXXX, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                        XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
     XXXXXXX, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,                        XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
-                                        KC_LPRN, KC_RPRN, KC_UNDS,      _______, XXXXXXX, XXXXXXX
+                                        KC_LPRN, KC_UNDS, KC_RPRN,      XXXXXXX, _______, XXXXXXX
   ),
 
   [_FUN] = LAYOUT_split_3x6_3(
     XXXXXXX, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_SYRQ,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX,
     XXXXXXX, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,                        XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
     XXXXXXX, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS,                        XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, XXXXXXX, XXXXXXX,
-                                        KC_APP,  KC_SPC,  KC_TAB,       XXXXXXX, XXXXXXX, _______
+                                        KC_APP,  KC_TAB, KC_SPC,        XXXXXXX, XXXXXXX, _______
   )
 
 };
